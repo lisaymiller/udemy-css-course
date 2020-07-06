7863345797e88afeb8fa190f882ed1e76f5e5038
Lisa Miller <lisayevonne@outlook.com>
2020-07-06 08:45:06 -0400

    notes and final udemy project

---
+++ main.js
@@ -0,0 +1,11 @@
+function buttonFunction() {
+  var button = document.getElementById("button");
+  button.style.display = "block";
+}
+
+function closeFunction() {
+  var button = document.getElementById("button");
+  button.style.display = "none";
+}
+
+// can also add event listener, click, on element
